75684d3463370a8cc222552fcfa2792235fc7dbb
Patrick Sherrard <patsherrard@gmail.com>
2020-07-22 14:43:58 -0400

    added button

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import './App.css';
 class App extends Component {
   constructor(props) { 
     super(props); 
-    this.state =  
-    isOn = false
+    this.state = {
+      isOn:false,
+    }  
   }
 
   buttonClick = () => {
     this.setState({
-      isOn = true
+      isOn:true
     })
   }
 
@@ -21,7 +22,12 @@ class App extends Component {
       <div className="App">
         <p>Hello there, This is my React App. Trying to use Travis CI for this project</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus esse placeat atque veniam dolorem voluptatem, magnam, laborum aperiam nostrum praesentium veritatis accusamus id quod cum! Minima perferendis omnis ipsam ea?</p>
-        <button onClick={this.buttonClick}></button>
+        <button onClick={this.buttonClick}>Click me</button>
+        {this.state.isOn
+        ? <div> I have been clicked! </div>
+        : null
+        
+        }
       </div>
     );
   }
