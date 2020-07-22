3e6afed4972760c80ef8e1f8e9cb1154c94d050c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-22 18:45:15 +0000

    Deploy patrickSherrard/my-react-app to github.com/patrickSherrard/my-react-app.git:gh-pages

---
+++ precache-manifest.850bdb0bf29fbfecc41aa9869fdb10b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "711ee69c69a2b1a914ca752dd0f03b70",
+    "url": "/my-react-app/index.html"
+  },
+  {
+    "revision": "0280919f7ff18bf16a6f",
+    "url": "/my-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9d252ec61298f5f742af",
+    "url": "/my-react-app/static/js/2.8c0c9318.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-react-app/static/js/2.8c0c9318.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0280919f7ff18bf16a6f",
+    "url": "/my-react-app/static/js/main.416090c8.chunk.js"
+  },
+  {
+    "revision": "435064591b6543e8ca17",
+    "url": "/my-react-app/static/js/runtime-main.c0584337.js"
+  }
+]);
