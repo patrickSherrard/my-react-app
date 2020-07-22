6dcf43a5a5fee3ec0612b538ecb8b7a85887d146
Patrick Sherrard <patsherrard@gmail.com>
2020-07-22 13:47:19 -0400

    is it going to pass?

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <p>Hello there, This is my React App. Trying to use Travis CI for this project</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus esse placeat atque veniam dolorem voluptatem, magnam, laborum aperiam nostrum praesentium veritatis accusamus id quod cum! Minima perferendis omnis ipsam ea?</p>
     </div>
   );
 }
